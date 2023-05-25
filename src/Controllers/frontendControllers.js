0abc374a607139977ae336eff726fcdef63c8194
yajindra.gautam <yajindra.gautam@ekbana.info>
2023-05-25 08:32:23 +0545

    implement project and instagram post image fetch

--- src/Controllers/frontendControllers.js
+++ src/Controllers/frontendControllers.js
@@ -1,7 +1,30 @@
+const fetch = require('node-fetch');
+const axios = require('axios');
+
 // Load Home Page
-exports.home = (req, res) => {
+exports.home = async (req, res) => {
   try {
-    return res.render("frontend/index.ejs");
+    const accessToken = process.env.TOKEN; // Retrieve the access token from the request query parameters
+    console.log('Checking token->',accessToken);
+    // Make a request to the Instagram API to fetch your photos
+    const response = await axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=${accessToken}`);
+
+    // Extract the necessary data from the API response
+    const photos = response.data.data.map(photo => ({
+      id: photo.id,
+      caption: photo.caption,
+      mediaUrl: photo.media_url
+    }));
+    const images = [];
+    photos.forEach((photo) => {
+      if(!photo.mediaUrl.includes('mp4')){
+        if(images.length != 20){
+        images.push(photo);
+        }
+      }
+    });
+    // Send the photos as the response
+    return res.render("frontend/index.ejs",{photos:images});
   } catch (err) {
     console.log(err);
   }
