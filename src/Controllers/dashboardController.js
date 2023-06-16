b3f81cf184fc76b9b5cbdedd6b8914aaa74a68ca
yajindra.gautam <yajindra.gautam@ekbana.info>
2023-06-16 07:14:38 +0545

    setup cms UI

--- src/Controllers/dashboardController.js
+++ src/Controllers/dashboardController.js
@@ -1,6 +1,63 @@
+const User = require("../models/users");
+
+// Render login page
 exports.loadDashboard = (req, res) => {
     try {
         return res.render('backend/pages/dashboard.ejs');
+    } catch (err) {
+        console.log(err);        
+    }
+};
+
+// Render profile overview page
+exports.loadOverview = async(req, res) => {
+    try {
+        const data = await User.findOne({email:"yajindragtm@gmail.com"});
+        const bradcrum ={
+            title:"Account Overview",
+            url: "/overview",
+            short: "account"
+        }
+        return res.render('backend/pages/overview.ejs',{data,bradcrum});
+    } catch (err) {
+        console.log(err);        
+    }
+};
+
+// Render profile setting page
+
+exports.loadProfileSetting = async(req, res) => {
+    try {
+        const data = await User.findOne({email:"yajindragtm@gmail.com"});
+        const bradcrum ={
+            title:"Account Setting",
+            url: "/profile-setting",
+            short: "setting"
+        }
+        return res.render('backend/pages/setting.ejs',{data, bradcrum});
+    } catch (err) {
+        console.log(err);        
+    }
+};
+
+exports.updateProfileIno = async(req, res) => {
+    try {
+        const userData = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            company: req.body.company,
+            phone: req.body.phone,
+            nationality: req.body.phone,
+            earning: req.body.earning,
+            success_rate: req.body.success_rate,
+        }
+        const data = await User.findOne({email:"yajindragtm@gmail.com"});
+        const bradcrum ={
+            title:"Account Setting",
+            url: "/profile-setting",
+            short: "setting"
+        }
+        return res.render('backend/pages/setting.ejs',{data, bradcrum});
     } catch (err) {
         console.log(err);        
     }
--- src/Routes/backend/dashboard.js
+++ src/Routes/backend/dashboard.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const Route = express.Router();
 const isLoggedIn = require('../../middlewares/isLoggedIn');
-const {loadDashboard} = require('../../Controllers/dashboardController');
+const {loadDashboard, loadOverview,loadProfileSetting,updateProfileIno} = require('../../Controllers/dashboardController');
 
 
 // GET: dashboard
-Route.get('/dashboard',[isLoggedIn],loadDashboard);
+Route.get('/dashboard',loadDashboard);
+
+// GET: profile overview
+Route.get("/overview",loadOverview);
+
+// GET: setting
+Route.get("/profile-setting",loadProfileSetting);
+
+//
+Route.put("/profile-setting",updateProfileIno);
 
 
 module.exports = Route;
